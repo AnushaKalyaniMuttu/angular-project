6bfbddc0ec13415ef54f126fe54b908a56ba281a
jyothimukala519 <53428898+AnushaKalyaniMuttu@users.noreply.github.com>
2024-11-10 23:07:42 +0530

    interpolation and attribute Binding

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,13 @@
+export const DUMMY_USERS=
+[
+    {
+    id:1,
+    name: "anusha",
+    avatar: "1.png",
+    },
+    {
+        id:2,
+        name: "kalyani",
+        avatar: "2.png",
+        }
+]
